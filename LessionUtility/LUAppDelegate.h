ba08249fa986023d5c8132cb3804694bda7de5dd
256 <256@256tekiMacBook-Pro.local>
2014-03-29 22:07:17 +0800

    Initial Commit

---
+++ LessionUtility/LUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LUAppDelegate.h
+//  LessionUtility
+//
+//  Created by 256 on 3/29/14.
+//  Copyright (c) 2014 256. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LessionUtility/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LessionUtility
+//
+//  Created by 256 on 3/29/14.
+//  Copyright (c) 2014 256. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LUAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LUAppDelegate class]));
+    }
+}
